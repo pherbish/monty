005101d9595d2ec94526104aca5ceaf5c7003a15
pherbish <pherbishorwa@gmail.com>
2024-01-21 06:20:45 -0800

    pall and push

--- pint.c
+++ pint.c
@@ -10,8 +10,8 @@ void f_pint(stack_t **head, unsigned int counter)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(vline.file);
-		free(vline.vhold);
+		fclose(bus.file);
+		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
